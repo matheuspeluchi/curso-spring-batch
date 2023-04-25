e15c0c8bbc7e385c06b56ecb3610e6af2b091994
Matheus Peluchi <matheuspeluchi@gmail.com>
2023-04-25 15:39:07 -0400

    Remove sensitive infos

--- src/main/java/com/mpr/cursobatch/config/BatchConfig.java
+++ src/main/java/com/mpr/cursobatch/config/BatchConfig.java
@@ -3,11 +3,15 @@ package com.mpr.cursobatch.config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -17,25 +21,26 @@ public class BatchConfig {
 
   @Bean
   public Job printMessage(JobRepository jobRepository, Step step) {
-    return new JobBuilder("print-message", jobRepository).start(step).build();
+    return new JobBuilder("print-message", jobRepository).start(step)
+        .incrementer(new RunIdIncrementer()).build();
   }
 
 
   @Bean
   public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-    return new StepBuilder("step1", jobRepository)
-        .tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
-          this.printHello();
-          return RepeatStatus.FINISHED;
-        }, transactionManager).build();
+    return new StepBuilder("step1", jobRepository).tasklet(printHello(null), transactionManager)
+        .build();
   }
 
 
-  private void printHello() {
-    System.out.println("#############################");
-    System.out.println("#############################");
-    System.out.println("Esta é a mensagem");
-    System.out.println("#############################");
-    System.out.println("#############################");
+  @Bean
+  @StepScope
+  public Tasklet printHello(@Value("#{jobParameters['nome']}") String name) {
+    return (StepContribution contribution, ChunkContext chunkContext) -> {
+      System.out.println(String.format("Olá, %s!", name));
+      return RepeatStatus.FINISHED;
+    };
   }
 }
+
+
