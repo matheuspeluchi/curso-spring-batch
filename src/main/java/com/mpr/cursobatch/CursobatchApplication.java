a35dfbecf37de983402282c489773da779b7dbb0
Matheus Peluchi <matheuspeluchi@gmail.com>
2023-04-25 14:27:40 -0400

    Initi

---
+++ src/main/java/com/mpr/cursobatch/CursobatchApplication.java
@@ -0,0 +1,13 @@
+package com.mpr.cursobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursobatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursobatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpr/cursobatch/CursobatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpr.cursobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursobatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
