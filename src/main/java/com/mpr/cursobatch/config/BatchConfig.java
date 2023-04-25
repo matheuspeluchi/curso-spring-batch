734867ebf1e8e0ab48e03b184ca6a7d6d0fccb95
Matheus Peluchi <matheuspeluchi@gmail.com>
2023-04-25 16:32:28 -0400

    Step chunk based

--- src/main/java/com/mpr/cursobatch/config/BatchConfig.java
+++ src/main/java/com/mpr/cursobatch/config/BatchConfig.java
@@ -1,5 +1,7 @@
 package com.mpr.cursobatch.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -10,6 +12,11 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -26,10 +33,21 @@ public class BatchConfig {
   }
 
 
+  // @Bean
+  // public Step printNameStep(JobRepository jobRepository, PlatformTransactionManager
+  // transactionManager) {
+  // return new StepBuilder("Setep-PrintName", jobRepository)
+  // .tasklet(printHello(null), transactionManager).build();
+  // }
+
+
   @Bean
-  public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-    return new StepBuilder("step1", jobRepository).tasklet(printHello(null), transactionManager)
-        .build();
+  public Step printEvenOrOddStep(JobRepository jobRepository,
+      PlatformTransactionManager transactionManager) {
+    return new StepBuilder("Setep-PrintEvenOrOdd", jobRepository)
+        .<Integer, String>chunk(0, transactionManager).reader(countReader())
+        .processor(numberProcessor()).writer(printWriter()).build();
+
   }
 
 
@@ -40,6 +58,21 @@ public class BatchConfig {
       System.out.println(String.format("Olá, %s!", name));
       return RepeatStatus.FINISHED;
     };
+  }
+
+
+  private IteratorItemReader<Integer> countReader() {
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    return new IteratorItemReader<>(numbers.iterator());
+  }
+
+  private FunctionItemProcessor<Integer, String> numberProcessor() {
+    return new FunctionItemProcessor<>((item -> item % 2 == 0 ? String.format("Item %s é Par", item)
+        : String.format("Item %s é Ímpar", item)));
+  }
+
+  private ItemWriter<String> printWriter() {
+    return itens -> itens.forEach((System.out::println));
   }
 }
 
